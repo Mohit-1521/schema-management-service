1f3f9c21713334fb9cb92c048d8408e22c677859
Mohit Sancheti <mohit.sancheti@flairlabs.com>
2025-05-20 15:12:14 +0530

    project-setup

---
+++ src/main/java/com/flairlabs/workflow/services/schema_mangement_service/SchemaMangementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flairlabs.workflow.services.schema_mangement_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemaMangementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaMangementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flairlabs/workflow/services/schema_mangement_service/SchemaMangementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flairlabs.workflow.services.schema_mangement_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemaMangementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
